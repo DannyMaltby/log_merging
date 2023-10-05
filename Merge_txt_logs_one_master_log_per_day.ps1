d6c0193fa84339c2447caa9d6d7b3783e4d2bcc6
DannyMaltby <147057346+DannyMaltby@users.noreply.github.com>
2023-10-05 14:47:45 +0100

    Update Merge_txt_logs_one_master_log_per_day.ps1

--- Merge_txt_logs_one_master_log_per_day.ps1
+++ Merge_txt_logs_one_master_log_per_day.ps1
@@ -12,5 +12,5 @@ $Files = (Get-ChildItem -Path $TargetDirectory -Recurse | where {$_.name -Like "
 #File Merge, purge redundant information, remove timestamp, then remove duplicate lines
 foreach ($File in $Files)
 {
-	(Get-Content $File) -notmatch "Excluded from processing by ID|Pupil records from arbor|for map returning|for map pupil from|for map save to csv file from|for map azure user from|PupilLeaver records from arbor|for map pupilLeaver from|for map pupilLeaverAzure from|Staff records from arbor with additional data|for map staff from C:|StaffFuture records from arbor with additional data|There were|StaffLeaver records from arbor with additional data|map staffLeaver from C:|map staffLeaverAzure from C:|for map arbor writeback from C:|PupilFuture records from arbor with additional data|map students by house|map staff by site udf|90 days deleted|switch function should have a default value if no case statement evaluates to true" | ForEach-Object {$_.substring(24,$_.Length -24)} |  sort | get-unique | Add-Content $DestinationFile
+	(Get-Content $File) -notmatch "{phrases that are unique to redundant lines that need removing, multiple entries seperated by a pipe '|'" | ForEach-Object {$_.substring(24,$_.Length -24)} |  sort | get-unique | Add-Content $DestinationFile
 }
