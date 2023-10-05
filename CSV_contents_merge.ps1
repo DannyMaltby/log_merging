69e2b500b5cd3113b2b20051f421ac09f6cc46dd
DannyMaltby <147057346+DannyMaltby@users.noreply.github.com>
2023-10-05 14:30:54 +0100

    Create CSV_contents_merge.ps1

---
+++ CSV_contents_merge.ps1
@@ -0,0 +1,16 @@
+#Where the file is written to
+$DestinationFile = "C:\Salamander\accounts\All Sites Accounts\Staff Accounts.csv"
+
+#Specify the files
+$TargetDirectory = "C:\Salamander\accounts"
+$Files = (Get-ChildItem -Path $TargetDirectory -Recurse | where {$_.name -Like "*-StaffMember*"}).FullName
+
+#Create File
+$Content = foreach ($File in $Files)
+{
+	Import-CSV $File | Select-Object *,@{Name="Site";Expression={"$File"}} | Export-CSV $DestinationFile -NoTypeInformation -Force -Append
+}
+
+#Fix File
+$RemoveDuplicates = Import-CSV $DestinationFile | Sort-Object * -Unique
+$RemoveDuplicates | Export-CSV $DestinationFile -NoTypeInformation -Force
