51ecb89b39f8bbea4a40d37e4c54de74ba85538f
DannyMaltby <147057346+DannyMaltby@users.noreply.github.com>
2023-10-05 14:52:41 +0100

    Update Merge_txt_logs_one_master_log_per_day.ps1

--- Merge_txt_logs_one_master_log_per_day.ps1
+++ Merge_txt_logs_one_master_log_per_day.ps1
@@ -3,10 +3,10 @@ $Date = Get-Date -Format "yyyyMMdd"
 $DateHumanReadable = Get-Date -Format "yyyyMMdd dddd dd-MM"
 
 #Where the file is written to
-$DestinationFile = "{Desired destination file}"
+$DestinationFile = "{Desired destination file path, with desired file name}"
 
 #Specify the files, this looks for files that include todays date in the format -yyyyMMdd
-$TargetDirectory = "{Top folder of all desired log files}"
+$TargetDirectory = "{Path to top folder of all desired log files}"
 $Files = (Get-ChildItem -Path $TargetDirectory -Recurse | where {$_.name -Like "*-$Date*"}).FullName
 
 #File Merge, purge redundant information, remove timestamp, then remove duplicate lines
